d8282e94af07c0fa6e136e1285b0310afc568d29
mc-es <mehmetcan.eser.dev@gmail.com>
2026-01-03 20:28:07 +0300

    refactor(logger): remove logger package and zap adapter, streamline logging setup in main application

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -7,42 +7,15 @@ import (
 	"syscall"
 
 	"go-auth/internal/config"
-	"go-auth/pkg/logger"
-	"go-auth/pkg/logger/zap"
 )
 
 func main() {
-	zapLogger, err := zap.New(zap.WithDevelopmentMode(), zap.WithoutStacktrace())
+	_, err := config.LoadEnv()
 	if err != nil {
 		panic(err)
 	}
 
-	logger.SetGlobalLogger(zapLogger)
-
-	defer func() {
-		_ = logger.Sync()
-	}()
-
-	envConfig, err := config.LoadEnv()
-	if err != nil {
-		logger.Fatal("Failed to load environment configuration", "error", err)
-	}
-
-	logger.Info(
-		"Application started",
-		"name",
-		envConfig.AppName,
-		"host",
-		envConfig.Host,
-		"port",
-		envConfig.Port,
-		"env",
-		envConfig.Env,
-	)
-
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 	<-sigChan
-
-	logger.Info("Shutting down...")
 }
